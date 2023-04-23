66f1902d9f255788a2015b189482ab4e3a7eda23
12368 <isaac@trevorandterri.com>
2023-04-22 18:07:31 -0700

    Created encoder datasheets for the Yellow Jacket Planetary Motors.

---
+++ TeamCode/src/main/java/main/util/encoders/GoBildaEncoderData5203Series.java
@@ -0,0 +1,17 @@
+package main.util.encoders;
+
+/** An encoder datasheet for the 5203 Series Yellow Jacket Planetary Gear Motors. */
+public class GoBildaEncoderData5203Series {
+    /** Variable convention: pulses per revolution (PPR), the RPM of the motor (RPM)*/
+    private final static double PPR_RPM30   = 5281.1;
+    private final static double PPR_RPM43   = 3895.9;
+    private final static double PPR_RPM60   = 2786.2;
+    private final static double PPR_RPM84   = 1993.6;
+    private final static double PPR_RPM117  = 1425.1;
+    private final static double PPR_RPM223  = 751.8;
+    private final static double PPR_RPM312  = 537.7;
+    private final static double PPR_RPM435  = 384.5;
+    private final static double PPR_RPM1150 = 145.1;
+    private final static double PPR_RPM1620 = 103.8;
+    private final static double PPR_RPM6000 = 28;
+}
